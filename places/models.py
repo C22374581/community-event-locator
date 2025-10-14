468b625866cb35e5d18a9fb4c7f3c34c271f0268
Jason Glancy <C22374581@mytudublin.ie>
2025-10-14 16:26:58 +0100

    feat: add spatial models (Neighborhood, Route, Event) with GiST indexes

--- places/models.py
+++ places/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=120)
+    area = models.PolygonField(srid=4326)
+
+    class Meta:
+        indexes = [models.GiSTIndex(fields=['area'])]
+
+    def __str__(self):
+        return self.name
+
+class Route(models.Model):
+    name = models.CharField(max_length=120)
+    path = models.LineStringField(srid=4326)
+
+    class Meta:
+        indexes = [models.GiSTIndex(fields=['path'])]
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    title = models.CharField(max_length=140)
+    description = models.TextField(blank=True)
+    when = models.DateTimeField()
+    location = models.PointField(srid=4326)
+    neighborhood = models.ForeignKey(
+        Neighborhood, null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    class Meta:
+        indexes = [models.GiSTIndex(fields=['location'])]
+
+    def __str__(self):
+        return self.title
+
